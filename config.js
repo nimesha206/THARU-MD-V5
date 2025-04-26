a2b8292994cdb6caba58dab6b81f4d75e434727e
۝⃟̥̩̩̩̥̩̥͚̮♥නිමේෂ♥😝™ࣩ࿔ࣰ🎓❁ཻ͜͡彡😄꧁⃟⃞❀༂❃⃟𝙉𝙄⃞𝙈𝙀𝙎⃟𝙃𝘼❃❀⃟꧂۝⃟̥̩̩̩̥̩̥͚̮ <121599526+nimesha206@users.noreply.github.com>
2025-04-26 23:39:57 +0530

    THARU MD V5

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ HEROKU_API: process.env.HEROKU_API || "HRKU-ed5c6fb8-14b9-4e21-b652-0df45300750f
 OWNER_NUMBER:process.env.OWNER_NUMBER || "",
 
 //Github Workflows Deploy කරනව නම් පමනක් පහත Env යෙදිය යුතුය!
-GITHUB_TOKEN: process.env.GITHUB_TOKEN || "",
-YOUR_REPO:process.env.YOUR_REPO || "",
+GITHUB_TOKEN: process.env.GITHUB_TOKEN || "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",
+YOUR_REPO:process.env.YOUR_REPO || "https://github.com/nimesha206/THARU-MD-V5.git",
 
 
 MAX_SIZE: 500,DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO};
